8b7a8fdc23830f4028849c90e8422f68733f9522
sures <sures@LAPTOP-8GGFC4AH>
2022-06-23 17:07:49 +0530

    23rd june 2022

---
+++ src/Five.java
@@ -0,0 +1,19 @@
+
+public class Five {
+ int a = 17;
+ int b = 15;
+ int cal(int a,int b) {
+ int result = a*b;
+ return result;
+ 
+ }
+ 
+ 
+	public static void main(String[] args) {
+	 Five FE = new Five ();
+	
+	int result = FE.cal(17,15);
+    System.out.println("total value" +result);
+
+}
+}
